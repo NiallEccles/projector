5c917bf332d8a3c5d422930d8ca622cfe458390b
Dev <n.eccles14@gmail.com>
2019-03-04 22:41:10 +0000

    Progress on outputting video.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 class Player {
-    ratio: string;
+    src: string;
     selector: string;
-    constructor(ratio, selector) {
-        this.ratio = ratio;
+    constructor(src, selector) {
+        this.src = src;
         this.selector = selector;
+        
+        const projectorRoot = document.querySelector(selector);
+        if(projectorRoot){
+            const projectorSrc = src;
+            const projector = `
+                <video>
+                    <source type="video/mp4" src="${projectorSrc}">
+                </video>
+            `;
+            projectorRoot.innerHTML = projector;
+        }
+        else{
+            console.error(`${selector} not found.`);
+        }
     }
     debug(){
-        console.log(this.ratio, this.selector)
+        console.log(this.src, this.selector)
     }
 }
 
-var myPlayer = new Player('16:9', 'player');
-myPlayer.debug();
+var myPlayer = new Player('https://cdn.jsdelivr.net/npm/big-buck-bunny-1080p@0.0.6/video.mp4', '.player');
