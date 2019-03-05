528a643549570e038d90cb19495e16a45f2b086c
Dev <n.eccles14@gmail.com>
2019-03-05 22:49:59 +0000

    Added in svg output. Tied button to play video.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ class Player {
     src: string;
     selector: string;
     projector: HTMLVideoElement;
+    playButton: any;
     constructor(src, selector) {
         this.src = src;
         this.selector = selector;
@@ -10,13 +11,29 @@ class Player {
         const projectorSrc = src;
 
         this.projector = document.createElement('video');
-        this.projector.src = projectorSrc
+        this.projector.src = projectorSrc;
+        // this.projector.setAttribute('controls', 'controls');
+
+        this.playButton = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        // this.playButton.appendChild(document.createElementNS(this.playButton.namespaceURI,'polygon'));
+        // this.playButton.setAttribute('points', '3 3 54.99 35 3 67 3 3');
+        const playButtonPolygon = document.createElementNS(this.playButton.namespaceURI,'polygon')
+        playButtonPolygon.setAttribute('points', '3 3 54.99 35 3 67 3 3');
+        playButtonPolygon.setAttribute('style', 'fill:#fff;stroke:#fff200;stroke-linecap:round;stroke-linejoin:round;stroke-width:6px;');
+        playButtonPolygon.addEventListener('click', () => {
+            this.play();
+        });
+        this.playButton.appendChild(playButtonPolygon);
+
+
+        
 
         projectorRoot.appendChild(this.projector);
+        projectorRoot.appendChild(this.playButton);
         this.projector.width =  this.projector.parentElement.clientWidth;
     }
     debug(){
-        console.log(this.src, this.selector)
+        console.log(this.src, this.selector, this.playButton)
     }
     play(){
         this.projector.play();
