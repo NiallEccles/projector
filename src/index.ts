4a9498825520171a48a76ff9e713804beed0a9d5
Dev <n.eccles14@gmail.com>
2019-03-06 20:58:04 +0000

    Added in click listener for projector.

--- src/index.ts
+++ src/index.ts
@@ -8,30 +8,36 @@ class Player {
     constructor(src, selector) {
         this.src = src;
         this.selector = selector;
+        this.isPlaying = false;
         
         const projectorRoot = document.querySelector(selector);
         const projectorSrc = src;
 
         this.projector = document.createElement('video');
         this.projector.src = projectorSrc;
-        // this.projector.setAttribute('controls', 'controls');
 
         this.playButton = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        this.playButton.setAttribute('id', 'playButton');
+
         const playButtonPolygon = document.createElementNS(this.playButton.namespaceURI,'polygon')
         playButtonPolygon.setAttribute('points', '3 3 54.99 35 3 67 3 3');
         playButtonPolygon.setAttribute('style', 'fill:none;stroke:#fff200;stroke-linecap:round;stroke-linejoin:round;stroke-width:6px;');
 
-        playButtonPolygon.addEventListener('click', () => {
-            this.play();
-        });
         this.playButton.appendChild(playButtonPolygon);
-
-
-        
 
         projectorRoot.appendChild(this.projector);
         projectorRoot.appendChild(this.playButton);
         this.projector.width =  this.projector.parentElement.clientWidth;
+
+        this.playButton.addEventListener('click', () => {
+            this.play();
+        });
+
+        this.projector.addEventListener('click', () => {
+            if(this.isPlaying){
+                this.pause();
+            }
+        });
     }
     debug(){
         console.log(this.src, this.selector, this.playButton)
