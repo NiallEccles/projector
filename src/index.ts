790b7023b9d7ca55c960256feb9b173d56b22324
Niall <nialle@rothco.ie>
2019-09-23 11:07:40 +0100

    Refactored code that creates container and inner.

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,7 @@ class Player {
     public createOverlay(){
         //stop scrolling
         $.body.style.cssText = 'overflow:hidden';
+        
         //create the overlay
         const overlay = $.createElement('div');
         overlay.setAttribute('id', 'ytvideoembed');
@@ -34,27 +35,16 @@ class Player {
         overlay.addEventListener('click', ()=>{
             this.stopVideo();
         });
-        //create the container, inner and button
-        //container
-        const container = $.createElement('div');
-        container.classList.add('yt-container');
-        container.style.cssText = 'position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%); text-align:center;';
-        //button
-        const button = $.createElement('button');
-        button.classList.add('yt-button');
-        button.innerHTML = 'Close';
-        button.addEventListener('click',()=>{
-            this.stopVideo();
-        });
-        //inner
-        const inner = $.createElement('div');
-        inner.setAttribute('id', 'player')
-        inner.style.cssText = 'margin-bottom:1em';
-        //append items
+        //append to body
         $.body.appendChild(overlay);
-        overlay.appendChild(container);
-        container.appendChild(inner);
-        container.appendChild(button);
+        //set the inner of overlay
+        overlay.innerHTML = 
+        `<div id="ytvideoembed" style="position:absolute;top:0;left:0;width:100vw;height:100vh;background:rgb(0, 0, 0, 0.9);">
+            <div class="yt-container" style="position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%); text-align:center;">
+                <div id="player"></div>
+                <button class="yt-button">Close</button>
+            </div>
+        </div>`;
         //start the YT video
         setTimeout(()=>{
             this.player =  new YT.Player('player', {
