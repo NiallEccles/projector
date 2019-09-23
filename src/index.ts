30a5db2d5298694fbf6f85e8e69a2f5dc7edad4d
Niall <nialle@rothco.ie>
2019-09-23 08:57:56 +0100

    YouTube video now plays.

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,6 @@ class Player {
     }
     public start(){
         this.createOverlay();
-        setTimeout(()=>{
-            this.player =  new YT.Player('player', {
-                videoId: this.src,
-                events: {
-                    'onReady': this.onPlayerReady
-                }
-            });
-        },3000)
     }
     public onPlayerReady(event){
         event.target.playVideo();
@@ -30,10 +22,18 @@ class Player {
         overlay.setAttribute('id', 'ytvideoembed');
         overlay.style.cssText = 'position:absolute;top:0;left:0;width:100vw;height:100vh;background:rgb(0, 0, 0, 0.6);';
         const container = document.createElement('div');
-        container.classList.add('player');
-        container.style.cssText = 'background: red;position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);';
+        container.setAttribute('id', 'player')
+        container.style.cssText = 'position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);';
         document.body.appendChild(overlay);
         overlay.appendChild(container);
+        setTimeout(()=>{
+            this.player =  new YT.Player('player', {
+                videoId: this.src,
+                events: {
+                    'onReady': this.onPlayerReady
+                }
+            });
+        });
     }
     public createScript(){
         if (!window['ytEmbedVideoScript']) {
